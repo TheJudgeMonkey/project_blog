4c9c035a29ca17beba2729feefbe75e8f3116f23
TheJudgeMonkey <mr.stas.anufriev@gmail.com>
2021-07-25 17:38:25 +0300

    Edit email put

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  before_save { self.email = eamil.downcase }
   has_many :articles
   validates :username, presence: true, 
     uniqueness: { case_sensitive: false }, 
